7df5a219f44028b2c853f201bc530f0c9fe54e64
cdwijayarathna <cdwijayarathna@gmail.com>
2015-04-30 14:56:26 +0530

    Basic Library Service Removed

--- lib/fibs.rb
+++ lib/fibs.rb
@@ -1,7 +1,7 @@
 module Fibs
     if RUBY_PLATFORM =~ /java/
       require File.expand_path('../fibs.jar', __FILE__)
-
+      Java::org.jruby.ext.FibsLibrary.new.load(JRuby.runtime, false)
       def self.mask(*args)
         @mask ||= Sequence.new
         @mask.mask(*args)
